8d96e42bf1b68e63d237738610f6f793800f76aa
ely211 <gonzalesm405@gmail.com>
2017-03-29 10:57:44 -0500

    redirecciona a cada pagina

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", function(){
+  document.getElementById("signUp").addEventListener("click", function(){
+    window.location.assign("signUp.html");
+  });
+  document.getElementById("signIn").addEventListener("click", function(){
+    window.location.assign("signIn.html");
+  });
+});
